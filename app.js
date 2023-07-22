b7b560382668e5956ebf0ef650a78534c78c9d32
include07 <j.elfirqi@gmail.com>
2023-07-23 00:42:31 +0100

    added game logic and animations

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-"using strict";
+"use strict";
+
 function getComputerChoice() {
   let x = window.Math.random();
   if (x < 0.3) {
@@ -12,7 +13,7 @@ function getComputerChoice() {
 
 function decide(computer, human) {
   if (human === "rock") {
-    if (computer === "rock ") {
+    if (computer === "rock") {
       return "tie";
     } else if (computer === "scissors") {
       return "human";
@@ -38,12 +39,94 @@ function decide(computer, human) {
   }
 }
 
-console.log(
-  "Hello human, u will be playing against the computer for a game of rock-papers-scissors, first to 3 is the winner, just write your choice when instructed to, goodluck!"
-);
+const paper = document.querySelector("#paperButton");
+const rock = document.querySelector("#rockButton");
+const scissors = document.querySelector("#scissorsButton");
+
+const paperRobot = document.querySelector("#paperRobot");
+const rockRobot = document.querySelector("#rockRobot");
+const scissorsRobot = document.querySelector("#scissorsRobot");
+
+const score = document.querySelector("#scoreTag");
+const comment = document.querySelector("#messageTag");
 
 let human = 0;
 let computer = 0;
+
+paper.addEventListener('click', function(e){
+  playRound("paper");
+})
+
+paper.addEventListener('mouseover', function(e){
+  paper.style.backgroundColor = 'green';
+})
+
+paper.addEventListener('mouseout', function(e){
+  paper.style.backgroundColor = '';
+})
+
+rock.addEventListener('click', function(e){
+  playRound("rock");
+})
+
+rock.addEventListener('mouseover', function(e){
+  rock.style.backgroundColor = 'green';
+})
+
+rock.addEventListener('mouseout', function(e){
+  rock.style.backgroundColor = '';
+})
+
+scissors.addEventListener('click', function(e){
+  playRound("scissors");
+})
+
+scissors.addEventListener('mouseover', function(e){
+  scissors.style.backgroundColor = 'green';
+})
+
+scissors.addEventListener('mouseout', function(e){
+  scissors.style.backgroundColor = '';
+})
+
+
+function playRound(humanChoice) {
+  // reset all robot choices
+  document.querySelectorAll('.robotChoice').forEach(choice => choice.classList.remove('animate'));
+
+  let computerChoice = getComputerChoice();
+  let result = decide(computerChoice, humanChoice);
+
+  // animate computer's choice
+  document.querySelector(`#${computerChoice}Robot`).classList.add('animate');
+
+  // display computer's choice
+  comment.textContent = `Computer chose ${computerChoice}`;
+
+  if (result === "human") {
+    human++;
+  } else if (result === "computer") {
+    computer++;
+  }
+
+  score.textContent = `${human} - ${computer}`;
+
+  // check for game end
+  if (human === 3 || computer === 3) {
+    let message = human === 3 ? 'Human Wins!' : 'Computer Wins!';
+    comment.textContent = message;
+    // reset game
+    human = 0;
+    computer = 0;
+    score.textContent = `${human} - ${computer}`;
+  }
+}
+
+
+
+/*
+
+
 
 while (human < 3 && computer < 3) {
   let x = window.prompt("its your turn, type your choice");
@@ -60,3 +143,4 @@ while (human < 3 && computer < 3) {
   }
   console.log("Human: " + human + "| Computer: " + computer);
 }
+*/
